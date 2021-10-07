b01ab8b4d5b34378c3fa8cf8231a90873484f9a0
Uvilager <vojvodaaleksandar@gmail.com>
2021-10-07 20:23:27 +0200

    fetch, svg

--- app.js
+++ app.js
@@ -1,54 +1,128 @@
-// document.getElementById('submit').addEventListener('click', getUsers);
-// let avatar = document.getElementById('avatar');
-// let credentials = document.getElementById('credentials');
-// let bio = document.getElementById('bio');
-// let repositories = document.getElementById('repositories');
-// let followers = document.getElementById('followers');
-// let following = document.getElementById('following');
-// let userLocation = document.getElementById('location');
-// let company = document.getElementById('company');
-// let twitter = document.getElementById('twitter');
-// let website = document.getElementById('website');
-// let user = document.querySelector('user');
+//Profile
+const avatar = document.getElementById('avatar');
+const credentials = document.getElementById('credentials');
+const user = document.querySelector('.user');
+const date = document.querySelector('.date');
+// Bio
+const bio = document.getElementById('bio');
+// Stats
+const repositories = document.getElementById('repositories');
+const followers = document.getElementById('followers');
+const following = document.getElementById('following');
+// Socials
+const userLocation = document.getElementById('location');
+const company = document.getElementById('company');
+const twitter = document.getElementById('twitter');
+const website = document.getElementById('website');
+// dark mode/ light mode
+const sun = document.querySelector('.sun');
+const moon = document.querySelector('.moon');
+const mode = document.querySelector('.mode');
 
-let sun = document.querySelector('.sun');
-let moon = document.querySelector('.moon');
-let mode = document.querySelector('.mode');
+document.getElementById('submit').addEventListener('click', getUsers);
 
-// function getUsers() {
-//     let username = document.getElementById('input').value;
-//     console.log(username);
+// Fetch
+function getUsers() {
+    let username = document.getElementById('input').value;
+    console.log(username);
 
-//     let gitUrl = `https://api.github.com/users/${username}`;
-//     console.log(gitUrl);
+    let gitUrl = `https://api.github.com/users/${username}`;
+    console.log(gitUrl);
 
-//     axios({
-//         method: 'get',
-//         url: gitUrl,
-//     })
-//         .then((response) => {
-//             showData(response);
-//             console.log(response);
-//         })
-//         .catch((error) => console.log(error));
-// }
+    axios({
+        method: 'get',
+        url: gitUrl,
+    })
+        .then((response) => {
+            showData(response);
+            console.log(response);
+        })
+        .catch((error) => console.log(error));
+}
 
-// function showData(response) {
-//     avatar.src = `${response.data.avatar_url}`;
-//     credentials.innerText = `${response.data.name}`;
-//     bio.innerText = `${response.data.bio}`;
-//     repositories.innerText = `${response.data.public_repos}`;
-//     followers.innerText = `${response.data.followers}`;
-//     following.innerText = `${response.data.following}`;
-//     userLocation.innerText = `${response.data.location}`;
-//     company.innerText = `${response.data.company}`;
-//     twitter.innerText = `${response.data.twitter_username}`;
-//     website.innerText = `${response.data.blog}`;
-//     user.innerText = `@${response.data.login}`;
-//     document.getElementById('res').innerHTML = `
-//     <h1>React>Vue: ${response.data.company}</h1>
-//     `;
-// }
+// On load
+const loadOctocat = async () => {
+    try {
+        await axios
+            .get('https://api.github.com/users/octocat')
+            .then((response) => {
+                showData(response);
+                console.log(response);
+            });
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+loadOctocat();
+
+// Show response.data
+function showData(response) {
+    //Profile
+    avatar.src = `${response.data.avatar_url}`;
+    credentials.innerText = `${response.data.name}`;
+    user.innerHTML = `@${response.data.login}`;
+
+    //Date
+    const months = [
+        'Jan',
+        'Feb',
+        'Mar',
+        'Apr',
+        'May',
+        'Jun',
+        'Jul',
+        'Aug',
+        'Sep',
+        'Oct',
+        'Nov',
+        'Dec',
+    ];
+
+    let createdAt = new Date(`${response.data.created_at}`);
+    console.log(createdAt);
+    const year = createdAt.getFullYear().toString();
+    const month = createdAt.getMonth().toString();
+    const day = createdAt.getUTCDate();
+    date.innerText = `Joined ${day} ${months[month]} ${year}`;
+
+    // Bio
+    if (response.data.bio === null) {
+        bio.innerText = 'This profile has no bio';
+    } else {
+        bio.innerText = `${response.data.bio}`;
+    }
+
+    // Stats
+    repositories.innerText = `${response.data.public_repos}`;
+    followers.innerText = `${response.data.followers}`;
+    following.innerText = `${response.data.following}`;
+
+    // Socials
+    if (response.data.location === null) {
+        userLocation.innerText = 'Not available';
+    } else {
+        userLocation.innerText = `${response.data.location}`;
+    }
+
+    if (response.data.company === null) {
+        company.innerText = 'Not available';
+    } else {
+        company.innerText = `${response.data.company}`;
+    }
+
+    if (response.data.twitter_username === null) {
+        twitter.innerText = 'Not available';
+    } else {
+        twitter.innerText = `${response.data.twitter_username}`;
+    }
+
+    if (response.data.blog === null) {
+        website.innerText = 'Not available';
+    } else {
+        website.innerText = `${response.data.blog}`;
+    }
+}
 
 // dark mode/ light mode
 mode.onclick = function () {
